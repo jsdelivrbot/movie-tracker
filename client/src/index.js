941005b7db74aa1d349362140aa21b548926d1ec
Patrick Rivera <patrick@Patricks-MacBook-Pro.local>
2018-03-18 16:13:00 -0400

    Brought in test boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App.jsx';
+import RequireAuth from './components/RequireAuth.jsx';
+import Resources from './components/Resources.jsx';
+import reducers from './redux';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router>
+      <App>
+        <Route path="/resources" component={RequireAuth(Resources)} />
+      </App>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
