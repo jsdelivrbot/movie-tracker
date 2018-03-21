1592b271fdeb423b98139d4bd93b30f68ed3a52a
Patrick Rivera <pxr13@my.fsu.edu>
2018-03-21 10:27:58 -0400

    Updated form styles

--- client/src/components/Auth/Form/FormStyles.jsx
+++ client/src/components/Auth/Form/FormStyles.jsx
@@ -4,7 +4,7 @@ import { Card } from 'material-ui/Card';
 
 export const StyledCard = styled(Card)`
   width: 400px;
-  height: 350px;
+  height: ${(props) => props.theme.height};
   margin: 5rem auto 0;
   padding: 2rem;
   display: flex;
@@ -18,4 +18,10 @@ export const FieldWrapper = styled.div`
 export const errorStyle = {
   position: 'absolute',
   marginBottom: '-.6rem'
+};
+
+StyledCard.defaultProps = {
+  theme: {
+    height: '300px'
+  }
 };
